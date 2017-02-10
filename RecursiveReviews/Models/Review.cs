5ff345b37832d48281c4371ff747292de38c5c5a
Ryan Bender <dyrshavon@gmail.com>
2017-02-10 14:29:01 -0500

    first commit

---
+++ RecursiveReviews/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecursiveReviews.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+        public int ReviewScore { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewAuthor { get; set; }
+        public string ReviewContent { get; set; }
+        public DateTime ReviewDate { get; set; }
+
+    }
+}
