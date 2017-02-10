311b3504c6c9d167c50053c8199f3e3e8803343e
Ryan Bender <dyrshavon@gmail.com>
2017-02-10 15:02:05 -0500

    first commit after doing a quick test on the databases

---
+++ RecursiveReviews/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecursiveReviews.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Place { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
